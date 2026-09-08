83b18acce270e43eb9845aa75ea58d041b4a759f
patquesa <patquesa@DESKTOP-LFLJTG9>
2026-09-08 19:20:11 +0200

    feat(auth): add validation DTOs for register and login

---
+++ backend/src/auth/dto/register-user.dto.ts
@@ -0,0 +1,21 @@
+//Register-User
+//Importamos las herramientas esenciales de validacion de la libreria class-validator
+import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
+
+//creamos una clase con los datos obligatorios para el registro
+export class RegisterUserDto {
+	@IsEmail({}, { message: 'Invalid email address'})
+	email!: string;
+
+	@IsString({ message: 'Username must be a string' })
+	@MinLength(3, { message: 'Username must be at least 3 characters long'})
+	username!: string;
+
+	@IsString({ message: 'Password must be a string' })
+	@MinLength(8, { message: 'Password must be at least 8 characters long' })
+	//Al menos \d numero o \W+ simbolo/especial, [A-Z] al menos una MAY y [a-z] al menos una Min
+	@Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+		message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number or symbol' 
+	})
+	password!: string;
+}
