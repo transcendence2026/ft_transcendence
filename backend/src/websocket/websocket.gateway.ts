fe81714296d9cbe7b6a37874f01572aa57407f5b
aboussem <aboussem@student.42malaga.com>
2026-09-08 12:33:06 +0200

    feat: update WebSocket gateway to handle JWT authentication and add room management services

--- backend/src/websocket/websocket.gateway.ts
+++ backend/src/websocket/websocket.gateway.ts
@@ -1,43 +1,79 @@
 import { Logger } from '@nestjs/common';
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import WebSocket from 'ws';
-import { PresenceService } from './websocket.service.js';
+import { PresenceService, RoomService } from './websocket.service.js';
+import jwt from 'jsonwebtoken';
+
+export type Client = {
+  socket: WebSocket;
+  userId: string;
+};
+
+type MessagePayload = {
+  roomName: string, 
+  message: string
+}
+
 
 @WebSocketGateway()
 export class WebsocketGateway {
-  constructor(private readonly presenceService: PresenceService) {}
+  constructor(
+    private readonly presenceService: PresenceService,
+    private readonly roomService: RoomService,
+  ) {}
   private readonly logger = new Logger(WebsocketGateway.name);
 
-  handleConnection(client: WebSocket, request: any) {
-    const token = request.headers.authorization
+  handleConnection(client: Client, request: any) {
+      const token = request.headers.authorization
 
-    if (!token) {
-      client.close(1008, "Token manquant")
-      return;
-    }
-    const userId = "test_user_id";
+      if (!token) {
+        client.socket.close(1008, "Token manquant")
+        return;
+      }
+    try {
+      const payload = jwt.verify(token, process.env.JWT_SECRET as string);
+      
+      if (typeof payload === 'string' || !payload.sub) {
+      client.socket.close(1008, "Token manquant")
+        throw new Error('Token invalido');
+      }
 
-    this.presenceService.addClient(userId, client);
-
-    this.logger.log('Un nouveau client vient de se connecter en WS.');
-
-    client.send('Bienvenue sur le WebSocket !');
-  }
-
-  handleDisconnect(client: WebSocket) {
-    const userId = "test_user_id";
-
-    this.presenceService.removeClient(userId, client);
+      const userId = payload.sub.toString();
+      
+      client.userId = userId;
+      this.presenceService.addClient(userId, client.socket);
+      this.logger.log('Un nuevo cliente se ha conectado.');
     
+      client.socket.send('Bienvenido al WebSocket !');
+    } catch {
+      client.socket.close(1008, 'Token invalido');
+    }
+  }
+  
+  handleDisconnect(client: Client, request: any) {
+
+    this.presenceService.removeClient(client.userId, client.socket);
+
     this.logger.log('Le client a fermé la page ou perdu la connexion.');
   }
 
+  broadcastToRoom(roomName: string, message: string) {
+    const userList = this.roomService.getUsersInRoom(roomName)
+
+    userList?.forEach((userId) => {
+      this.presenceService.getUserSockets(userId)?.forEach((socket) => socket.send(message));
+    })
+  }
+
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string, @ConnectedSocket() client: WebSocket) {
-    const text = typeof message === 'string' ? message : JSON.stringify(message);
+  handleMessage(@MessageBody() data: MessagePayload) {
+    this.broadcastToRoom(data.roomName, data.message)
+  }
 
-    this.logger.log(`Reçu du client : ${text}`);
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(@MessageBody() data: MessagePayload, @ConnectedSocket() client: Client) {
 
-    client.send(`J'ai bien reçu ton message : "${text}"`);
+    this.roomService.joinRoom(client.userId, data.roomName)
+
   }
 }
