199dc2c8205a68ddf4f6e7f8905a1a830cdbd8eb
patquesa <patquesa@DESKTOP-LFLJTG9>
2026-09-09 21:16:07 +0200

    feat(app): add initial main.ts skeleton

---
+++ backend/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from
+import { AppModule } from
+
+//bootstrap es la funcion encargada de arrancar la aplicacion
+//enciende el motor conectando el AppModule
+async function bootstrap() {
+
+}
