6555b68927f9b66209a129d30bee1b629409a565
patquesa <patquesa@student.42malaga.com>
2026-09-10 12:34:37 +0200

    chore: verify backend startup and main.ts configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from
-import { AppModule } from
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common'; 
+import { AppModule } from './app.module.js';
 
 //bootstrap es la funcion encargada de arrancar la aplicacion
 //enciende el motor conectando el AppModule
 async function bootstrap() {
-
+    const app = await NestFactory.create(AppModule);
+    //Habilita las reglas de validacion que hay en los DTOs (que datos esten bien formateados)
+    //ValidationPipe revisa por ej que la contraseña cumple requisitos minimos de seguridad
+    app.useGlobalPipes(new ValidationPipe());
+    // Aquí usamos await porque crear la app y abrir el puerto de red toma unos milisegundos
+    await app.listen(3000);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
+bootstrap();
