ed3a9d17ac6c45d4ea784ee1dedd89f5727d2db4
patquesa <patquesa@student.42malaga.com>
2026-09-10 13:16:21 +0200

    feat(auth): implement bcrypt password hashing and support custom email domains

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { RegisterUserDto } from './dto/register-user.dto.js';
 import { LoginUserDto } from './dto/login-user.dto.js';
 
 //declara y exporta la clase de servicio dd reside la logica de autenticacion
 @Injectable()
 export class AuthService {
+	//Numero de rondas de salt estandar
+	private readonly saltRounds = 10;
 	//Recibe los datos validados del registro
 	async register(registerDto: RegisterUserDto) {
+		const { email, username, password } = registerDto;
+
+		//Ciframos contraseña con bcrypt
+		const hashedPassword = await bcrypt.hashSync(password, this.saltRounds);
+		
 		// Más adelante aquí llamaremos a la base de datos para guardar el usuario
 		//exige q registerDto contenga campos validados por class-validator
 		return {
 			message: 'User registered successfully',
 			user: {
-				email: registerDto.email,
-				username: registerDto.username,
+				email,
+				username,
+				hashedPassword, // Lo devolvemos temporalmente para verificar el hash en las pruebas
 			},
 		};
 	}
--- backend/src/auth/dto/register-user.dto.ts
+++ backend/src/auth/dto/register-user.dto.ts
@@ -4,7 +4,11 @@ import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
 
 //creamos una clase con los datos obligatorios para el registro
 export class RegisterUserDto {
-	@IsEmail({}, { message: 'Invalid email address'})
+	//@IsEmail({}, { message: 'Invalid email address'})
+	//Esta expresión valida cualquier estructura de correo estándar y además permite dominios personalizados
+	@Matches(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, {
+        message: 'Invalid email address format',
+	})
 	email!: string;
 
 	@IsString({ message: 'Username must be a string' })
