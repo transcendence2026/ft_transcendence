94a83dc4ef78efcaa59e6144008677dd7710354e
patquesa <patquesa@student.42malaga.com>
2026-09-14 12:37:50 +0200

    docs(auth): add explanatory comments to authentication module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
+//PrismaModule da acceso a la Base de Datos
 import { PrismaModule } from '../prisma/prisma.module.js';
 
 //Registras y juntas el AuthController mas AuthService y se guardan en el NestJS
@@ -10,12 +11,15 @@ import { PrismaModule } from '../prisma/prisma.module.js';
 	imports: [
 		PrismaModule,
     	JwtModule.register({
-      		secret: process.env.JWT_SECRET || 'super-secret', // O tu config de entorno
-      		signOptions: { expiresIn: '1d' },
+      		secret: process.env.JWT_SECRET || 'super-secret', // define contraseña maestra para firmar token
+      		signOptions: { expiresIn: '1d' }, //token caduca tras 1 día
     	}),
 	],
+	//Registras el controler y el service para que NestJS sepa de su existencia
 	controllers: [AuthController],
 	providers: [AuthService],
 })
 export class AuthModule {}
 //al AuthModule se exporta, luego sera importado por app.module.ts
+//Cuando arranca el servidor, NestJS lee appModule que a su vez importa AuthModule
+//si no lo hiciera no sabria las rutas de registro y login
