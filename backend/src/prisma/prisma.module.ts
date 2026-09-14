83f90fec5de6e2d37d2fbb7e3880c63a63d35548
patquesa <patquesa@student.42malaga.com>
2026-09-14 13:41:15 +0200

    docs(prisma): add explanatory comments to prisma modules

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,7 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service.js';
 
-@Global() // Opcional: hace que no tengas que importarlo en cada módulo
+//@Global Convierte a PrismaModule en un módulo global.
+//pasa a estar disponible automáticax en todos los demás módulos (como AuthModule, UsersModule, etc.)
+//sin necesidad de tener que importarlo explícitamente en cada uno de ellos.
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
